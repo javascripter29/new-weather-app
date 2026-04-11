4a0b2029d291de5efb46188846ba23b06ba24b3b
unknown <2009.12illya@gmail.com>
2026-04-11 16:22:02 +0300

    fix bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { AppProviders } from './providers/AppProviders/AppProviders.jsx'
-import 'antd/dist/reset.css'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { AppProviders } from "./providers/AppProviders/AppProviders.jsx";
+import "antd/dist/reset.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProviders>
       <App />
     </AppProviders>
   </StrictMode>,
-)
+);
+// hello
