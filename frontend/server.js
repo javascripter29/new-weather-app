710ec50f6781ec14f29891500a33adee8934b4a0
unknown <2009.12illya@gmail.com>
2026-04-26 12:51:06 +0300

    fix bugs 9

--- frontend/server.js
+++ frontend/server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 80;
 
 // Serve static files from dist
 app.use(express.static(path.join(__dirname, "dist")));
